8abbafab147a4d4c7b0d4fc202da24bfa134d563
amar1508 <66547732+amar1508@users.noreply.github.com>
2020-06-10 06:17:27 +0530

    readme

---
+++ makecachematrix.R
@@ -0,0 +1,43 @@
+ makeCachematrix<-function(x){
+ inv<-NULL
+set<-function(y){
+x<<-y
+inv<<- NULL
+}
+get<-function(){x}
+setinverse<-function(inverse){inv<<-inverse}
+getinverse<-function(){inv}
+list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+
+}
+cachesolve<- function(x,...){
+inv<-x$getinverse()
+if(!is.null(inv)){
+message("getting cached data")
+return(inv)
+}
+mat<-x$get()
+inv<<-solve(mat,...)
+x$setinverse(inv)
+inv
+}
+
+
+
+
+
+
+
+
+
+
+
+pmatrix<-makeCachematrix(matrix(1:16,nrow=4,ncol=4))
+pmatrix$get()
+pmatrix$getinverse()
+cachesolve(pmatrix)
+
+pmatrix<-makeCachematrix(matrix(1:4,nrow=2,ncol=2))
+pmatrix$get()
+pmatrix$getinverse()
+cachesolve(pmatrix)
